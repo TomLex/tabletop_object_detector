da9f490d0a559e0be434882292ea6d3d1fe4a9d9
Tomas Varga <xvarga00@stud.fit.vutbr.cz>
2015-04-25 03:22:05 +0200

    added json output for python processing

--- src/ping_tabletop_node.cpp
+++ src/ping_tabletop_node.cpp
@@ -42,13 +42,19 @@
 #include "tabletop_object_detector/TabletopSegmentation.h"
 #include "tabletop_object_detector/TabletopObjectRecognition.h"
 
+#include <sensor_msgs/point_cloud_conversion.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/common/centroid.h>
+
+using namespace std;
+
 /*! Simply pings the tabletop segmentation and recognition services and prints out the result.*/
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "ping_tabletop_node");
   ros::NodeHandle nh;
 
-  std::string service_name("/tabletop_segmentation");
+  string service_name("/tabletop_segmentation");
   while ( !ros::service::waitForService(service_name, ros::Duration().fromSec(3.0)) && nh.ok() )
   {
     ROS_INFO("Waiting for service %s...", service_name.c_str());
@@ -66,8 +72,10 @@ int main(int argc, char **argv)
     ROS_ERROR("Segmentation service returned error %d", segmentation_srv.response.result);
     exit(0);
   }
-  ROS_INFO("Segmentation service succeeded. Detected %d clusters", (int)segmentation_srv.response.clusters.size());
+  //ROS_INFO("Segmentation service succeeded. Detected %d clusters", (int)segmentation_srv.response.clusters.size());
   if (segmentation_srv.response.clusters.empty()) exit(0);
+
+  //ROS_INFO("Table position: x_min:%f x_max:%f y_min:%f y_max:%f", segmentation_srv.response.table.x_min, segmentation_srv.response.table.x_max, segmentation_srv.response.table.y_min, segmentation_srv.response.table.y_max);
 
   service_name = "/tabletop_object_recognition";
   if ( !ros::service::waitForService(service_name, ros::Duration().fromSec(3.0)) )
@@ -92,6 +100,25 @@ int main(int argc, char **argv)
     exit(0);
   }
 
+  ROS_INFO("Table pose: x:%f, y:%f, z:%f", segmentation_srv.response.table.pose.pose.position.x,segmentation_srv.response.table.pose.pose.position.y,segmentation_srv.response.table.pose.pose.position.z);
+  ROS_INFO("Table orientation: x:%f, y:%f, z:%f", segmentation_srv.response.table.pose.pose.orientation.x,segmentation_srv.response.table.pose.pose.orientation.y,segmentation_srv.response.table.pose.pose.orientation.z, segmentation_srv.response.table.pose.pose.orientation.w);
+
+  for (size_t i=0; i<segmentation_srv.response.clusters.size(); i++) 
+  {
+    pcl::PointXYZ average_points = pcl::PointXYZ(0.0, 0.0, 0.0);
+    for (size_t j=0; j<segmentation_srv.response.clusters[i].points.size(); j++)
+    {
+      average_points.x += segmentation_srv.response.clusters[i].points[j].x;
+      average_points.y += segmentation_srv.response.clusters[i].points[j].y;
+      average_points.z += segmentation_srv.response.clusters[i].points[j].z;
+    }
+
+    average_points.x /= segmentation_srv.response.clusters[i].points.size();
+    average_points.y /= segmentation_srv.response.clusters[i].points.size();
+    average_points.z /= segmentation_srv.response.clusters[i].points.size();
+    cout << "XYZ: " << average_points.x << " " << average_points.y << " " << average_points.z << endl;
+  }
+
   ROS_INFO("Recognition results:");
   for (size_t i=0; i<recognition_srv.response.models.size(); i++)
   {
@@ -101,7 +128,8 @@ int main(int argc, char **argv)
     }
     else
     {
-      ROS_INFO("  Model id %d",recognition_srv.response.models[i].model_list[i].model_id);
+      ROS_INFO("  Model id %d",recognition_srv.response.models[i].model_list[0].model_id);
+      ROS_INFO("  Model pose: x:%f y:%f z:%f", recognition_srv.response.models[i].model_list[0].pose.pose.position.x, recognition_srv.response.models[i].model_list[0].pose.pose.position.y, recognition_srv.response.models[i].model_list[0].pose.pose.position.z);
     }
   }
 
@@ -111,5 +139,68 @@ int main(int argc, char **argv)
     ROS_INFO("  Cluster %u: recognition result %d", (unsigned int)i, 
              recognition_srv.response.cluster_model_indices.at(i));
   }
+
+  // JSON output, for python processing
+  cout << "{" << endl;
+  cout << "  \"clusters\": {" << endl;
+  for (size_t i=0; i<segmentation_srv.response.clusters.size(); i++)
+  {
+    cout << "    \"" << i << "\": {" << endl;
+    cout << "      \"average\": {" << endl;
+
+    pcl::PointXYZ average_points = pcl::PointXYZ(0.0, 0.0, 0.0);
+    for (size_t j=0; j<segmentation_srv.response.clusters[i].points.size(); j++)
+    {
+      average_points.x += segmentation_srv.response.clusters[i].points[j].x;
+      average_points.y += segmentation_srv.response.clusters[i].points[j].y;
+      average_points.z += segmentation_srv.response.clusters[i].points[j].z;
+    }
+    average_points.x /= segmentation_srv.response.clusters[i].points.size();
+    average_points.y /= segmentation_srv.response.clusters[i].points.size();
+    average_points.z /= segmentation_srv.response.clusters[i].points.size();
+
+    cout << "        \"x\":" << average_points.x << "," << endl;
+    cout << "        \"y\":" << average_points.y << "," << endl;
+    cout << "        \"z\":" << average_points.z << endl;
+    cout << "      }," << endl;
+    cout << "      \"models\": [" << endl;
+
+    for (size_t k=0; k<recognition_srv.response.models[i].model_list.size(); k++)
+    {
+      cout << "        {" << endl;
+      cout << "          \"model_id\": " << recognition_srv.response.models[i].model_list[k].model_id << "," << endl;
+      cout << "          \"pose\": {" << endl;
+      cout << "            \"x\": " << recognition_srv.response.models[i].model_list[k].pose.pose.position.x << "," << endl;
+      cout << "            \"y\": " << recognition_srv.response.models[i].model_list[k].pose.pose.position.y << "," << endl;
+      cout << "            \"z\": " << recognition_srv.response.models[i].model_list[k].pose.pose.position.z << endl;
+      cout << "          }," << endl;
+      cout << "         \"confidence\": " << recognition_srv.response.models[i].model_list[k].confidence << endl;
+      cout << "        }";
+      
+      if (k != recognition_srv.response.models[i].model_list.size() - 1)
+      {
+        cout << "," << endl;
+      }
+      else
+      {
+        cout << endl;
+      }
+    }
+    cout << "      ]" << endl;
+    cout << "    }";
+
+    if (i != segmentation_srv.response.clusters.size() - 1)
+    {
+      cout << "," << endl;
+    }
+    else
+    {
+      cout << endl;
+    }
+  }
+  cout << "  }" << endl;
+  cout << "}";
+
+
   return true;
 }
